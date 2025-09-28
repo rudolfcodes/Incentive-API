5a1a41e7097e01e67a7d596e3f0fcac7e4d2f7a1
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-28 13:02:19 +0100

    create user dto

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+@InputType()
+export class CreateUserInput {
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @Field()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+}
