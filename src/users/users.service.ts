9bbbac8374890c716d1d6c575f9df3f400d92002
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-28 13:08:11 +0100

    create users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserInput } from './dtos/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // The InjectRepository decorator is used to inject the User repository
+  constructor(
+    @InjectRepository(User) private usersRepository: Repository<User>,
+  ) {}
+
+  // create a new user
+  async create(createUserDto: CreateUserInput): Promise<User> {
+    // We will receive the user email and password from the DTO
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    // We will need to hash the password before saving it to the database
+    // Use bcrypt for hashing
+    const newUser = this.usersRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    return this.usersRepository.save(newUser);
+  }
+
+  // List all users by companyId
+  findAll(companyId: string) {
+    return this.usersRepository.find({ where: { companyId } });
+  }
+  // Find user by ID
+  findById(id: number) {
+    if (!id) {
+      throw new NotFoundException('User ID must be provided');
+    }
+    return this.usersRepository.findOneBy({ id });
+  }
+}
