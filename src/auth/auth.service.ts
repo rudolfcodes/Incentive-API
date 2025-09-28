2a3a050efdfb255e924d4d7bef8b571a9f44c348
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-28 22:15:16 +0100

    change validateUser to accept a user email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,8 +11,8 @@ export class AuthService {
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(userId: number, password: string): Promise<any> {
-    const user = await this.usersService.findById(userId);
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.usersService.findByEmail(email);
     if (user && (await bcrypt.compare(password, user.password))) {
       const { password, ...result } = user;
       return result;
