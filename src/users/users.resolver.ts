ba5b2f506aac1e822090c5cea616e7d459009081
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-28 13:13:22 +0100

    add mutation to resolver
    also moved the resolver

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,21 @@
+import { Args, Resolver, Query, Mutation } from '@nestjs/graphql';
+import { User } from 'src/users/user.entity';
+import { UsersService } from './users.service';
+import { CreateUserInput } from './dtos/create-user.dto';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query(() => [User], { name: 'users' })
+  @Mutation(() => User)
+  async createUser(
+    @Args('createUserInput') createUserInput: CreateUserInput,
+  ): Promise<User> {
+    return this.usersService.create(createUserInput);
+  }
+
+  getUsers() {
+    return this.usersService.findAll('companyId');
+  }
+}
