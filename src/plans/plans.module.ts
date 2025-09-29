7b32d1c87fadce32a3a4b52583bb69b29faf1fc3
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-29 20:37:58 +0100

    setup plans module

--- src/plans/plans.module.ts
+++ src/plans/plans.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Plan } from './plan.entity';
+import { CompaniesModule } from '../companies/companies.module';
+import { PlansService } from './plans.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Plan]), CompaniesModule],
+  providers: [PlansService],
+})
 export class PlansModule {}
