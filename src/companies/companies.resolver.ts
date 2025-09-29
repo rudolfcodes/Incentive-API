dafe41d340cc0f5b7b44afc2b669c15a390b5ce6
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-29 12:51:39 +0100

    apply guards to companies resolver

--- src/companies/companies.resolver.ts
+++ src/companies/companies.resolver.ts
@@ -2,12 +2,15 @@ import { Args, Resolver, Query, Mutation, Context } from '@nestjs/graphql';
 import { Company } from './company.entity';
 import { CompaniesService } from './companies.service';
 import { CreateCompanyInput } from './dtos/create-company-input.dto';
+import { UseGuards } from '@nestjs/common';
+import { GqlAuthGuard, SuperAdminGuard } from 'src/auth/gql-auth.guard';
 
 @Resolver((of) => Company)
 export class CompaniesResolver {
   constructor(private readonly companiesService: CompaniesService) {}
 
   @Mutation(() => Company)
+  @UseGuards(SuperAdminGuard)
   async createCompany(
     @Args('createCompanyInput') createCompanyInput: CreateCompanyInput,
   ): Promise<Company> {
@@ -15,17 +18,21 @@ export class CompaniesResolver {
   }
 
   @Query(() => [Company])
+  @UseGuards(SuperAdminGuard)
   async getCompanies(): Promise<Company[]> {
     return this.companiesService.findAll();
   }
 
   @Query(() => Company, { nullable: true })
+  @UseGuards(SuperAdminGuard)
   async getCompanyById(@Args('id') id: number): Promise<Company | null> {
     return this.companiesService.getCompanyById(id);
   }
 
   @Query(() => Company, { nullable: true })
-  async myCompany(): Promise<Company | null> {
-    return this.companiesService.myCompany();
+  @UseGuards(GqlAuthGuard)
+  async myCompany(@Context() ctx: { userId: number }): Promise<Company | null> {
+    const { userId } = ctx;
+    return this.companiesService.myCompany(userId);
   }
 }
