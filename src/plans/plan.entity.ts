f6c9fb095a8fb210178ece262b316f17c0ccbdd8
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-29 16:49:38 +0100

    created plan entity

---
+++ src/plans/plan.entity.ts
@@ -0,0 +1,87 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  Index,
+  JoinColumn,
+} from 'typeorm';
+import { ObjectType, Field, Int, registerEnumType, ID } from '@nestjs/graphql';
+import { Company } from 'src/companies/company.entity';
+
+export enum PlanType {
+  OPTION_ISO = 'OPTION_ISO',
+  OPTION_NSO = 'OPTION_NSO',
+  RSU = 'RSU',
+  PSU = 'PSU',
+  RSA = 'RSA',
+}
+export enum PlanStatus {
+  ACTIVE = 'ACTIVE',
+  INACTIVE = 'INACTIVE',
+  EXPIRED = 'EXPIRED',
+  COMPLETED = 'COMPLETED',
+}
+export enum VestInterval {
+  MONTHLY = 'MONTHLY',
+  QUARTERLY = 'QUARTERLY',
+  ANNUAL = 'ANNUAL',
+}
+
+registerEnumType(PlanType, {
+  name: 'PlanType',
+});
+registerEnumType(PlanStatus, {
+  name: 'PlanStatus',
+});
+registerEnumType(VestInterval, {
+  name: 'VestInterval',
+});
+
+@ObjectType({ description: 'Plan entity' })
+@Entity()
+export class Plan {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field((type) => String)
+  @Column({ length: 160 })
+  name: string;
+
+  @Field(() => PlanType)
+  @Column()
+  type: PlanType;
+
+  @Field((type) => String)
+  @Column()
+  description: string;
+
+  @Field(() => PlanStatus)
+  @Column({ default: PlanStatus.ACTIVE })
+  status: PlanStatus;
+
+  @Field()
+  @Index()
+  @Column()
+  companyId: number;
+  @ManyToOne(() => Company, (company) => company.plans, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: 'companyId' })
+  company: Company;
+
+  @Field(() => Date)
+  @Column({ type: 'datetime' })
+  startDate: Date;
+
+  @Field(() => Date)
+  @Column({ type: 'datetime' })
+  endDate: Date;
+
+  @Field({ nullable: true })
+  @Column({ type: 'datetime', nullable: true })
+  expiresAt: Date | null;
+
+  @Field(() => Int)
+  @Column()
+  totalShares: number;
+}
