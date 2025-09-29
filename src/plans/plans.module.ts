d9f05ba9c24a8d367460566d0afa6db829efcd87
Rudolf Cardol <r_cardol@hotmail.com>
2025-09-29 20:52:59 +0100

    added plansresolver to providers

--- src/plans/plans.module.ts
+++ src/plans/plans.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Plan } from './plan.entity';
 import { CompaniesModule } from '../companies/companies.module';
 import { PlansService } from './plans.service';
+import { PlansResolver } from './plans.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Plan]), CompaniesModule],
-  providers: [PlansService],
+  providers: [PlansService, PlansResolver],
 })
 export class PlansModule {}
